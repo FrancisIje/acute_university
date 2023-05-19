9818196467b83e11339aa4540f2f5cce04df8339
Francis <ijenebefrancis@gmail.com>
2023-05-19 17:56:22 +0100

    refactor

--- lib/Global/theme.dart
+++ lib/Global/theme.dart
@@ -2,19 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 ThemeData appTheme = ThemeData(
-  
-  inputDecorationTheme:  InputDecorationTheme(
-    border:const  OutlineInputBorder(
-      
-      borderSide: BorderSide.none,
-
-      borderRadius: BorderRadius.all(Radius.circular(5)),
-    ),
-    fillColor:const  Color(0xFFF2F2F2),
-    filled: true,
-    contentPadding: EdgeInsets.symmetric(vertical: 15.sp, horizontal: 15.sp)
-
-  ),
+  inputDecorationTheme: InputDecorationTheme(
+      border: const OutlineInputBorder(
+        borderSide: BorderSide.none,
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+      ),
+      fillColor: const Color(0xFFF2F2F2),
+      filled: true,
+      contentPadding: EdgeInsets.symmetric(vertical: 15.h, horizontal: 15.w)),
   textTheme: Typography.englishLike2018.apply(
     fontSizeFactor: 1.sp,
     fontFamily: "Sansation",
